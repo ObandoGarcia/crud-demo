1b7978b2c457f7a0de362384255c6ad6cf1ce71b
Jose Manuel Obando <obandogarcia10635@gmail.com>
2025-11-29 18:16:07 -0600

    Creacion de archivos base y entidad de Productos
    Creacion de entidades y repositorios de productos

---
+++ src/main/java/com/obando/crud_demo/model/Product.java
@@ -0,0 +1,39 @@
+package com.obando.crud_demo.model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "products")
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    public Long id;
+
+    private Double price;
+    private String description;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
